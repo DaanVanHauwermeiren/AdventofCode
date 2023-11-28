492ebbe4bbf581d0105b0d231d1aa6524657e4c7
DaanVanHauwermeiren <daan.van.hauwermeiren@gmail.com>
2023-11-28 14:01:50 +0100

    update test: now with execution time and some refactoring

--- test.sh
+++ test.sh
@@ -7,136 +7,113 @@
 
 # the python executable in this virtual environment
 PYTHON_EXEC=.venv/bin/python3
-echo $PYTHON_EXEC
 
-# python files
-# parsing ls is not a good idea
-# fns=$(ls ./code/*.py)
-# the correct solution is even simpler: just use glob
-fns="./code/*.py"
-echo $fns
-
-for SCRIPT_FN in $fns
-do
-    echo $SCRIPT_FN
-
-    # NOTE: this is super crappy programming, need to deal with this later
-    # string manipulation so that everything after the first underscore is cut off:
-    # ./code/DVH
-    DERP=${SCRIPT_FN%%_*}
-    # this get the number of letters in the string
-    # echo ${#DERP}
-    # we use the number of the sliced string above to slice the code file name
-    # and get someting of the form: YYYY_D or YYYY_DD
-    # echo ${SCRIPT_FN:${#DERP}+1:-3}
-    YEARDAY=${SCRIPT_FN:${#DERP}+1:-3}
-    # split again into the year and the day
-    YEAR=${YEARDAY%%_*}
-    DAY=${YEARDAY#*_}
-    # we add that slice to some other small string to get our data file location
-    DATAFILE="./data/input_"
-    DATAFILE+=$YEAR
-    DATAFILE+="_"
-    DATAFILE+=$DAY
-    DATAFILE+=".txt"
-
-    # add if clause on extension to determine which of the files things to execute
-    # specific for python
-    #################################################3
-    # run a command of the form:
-    # python file.py data.txt
-    # capturing the STDOUT from the python file execution
-    # something of the form:
-    # (13123, 2342424234)
-    RESULT=$($PYTHON_EXEC $SCRIPT_FN $DATAFILE)
-    echo $RESULT
-    # once again crappy programming to split the tuple into two integers
-    RESULT1=${RESULT%%, *}
-    RESULT1=${RESULT1:1}
-    echo $RESULT1
-    RESULT2=${RESULT#*, }
-    RESULT2=${RESULT2:0:-1}
-    echo $RESULT2
-
-    # query the solutions json file
-    SOLUTION1=$(cat ./data/solutions.json | jq -r ".\"$YEAR\".\"$DAY\"[0]")
-    SOLUTION2=$(cat ./data/solutions.json | jq -r ".\"$YEAR\".\"$DAY\"[1]")
-    # test correctness of solution.
-    if [ "$RESULT1" = "$SOLUTION1" ]; then
-        echo "Solution 1 is correct."
-    fi
-    if [ "$RESULT2" = "$SOLUTION2" ]; then
-        echo "Solution 2 is correct."
-    fi
-
-    # timing of execution
-    # add another if clause here for python and julia
-    # TIMEFORMAT=%E get real execution time in seconds
-    EXECTIME=$({ TIMEFORMAT=%E; time $PYTHON_EXEC $SCRIPT_FN $DATAFILE > /dev/null; } 2>&1)
-    echo $EXECTIME
-done
-
-
-
-# now julia
-fns="./code/*.jl"
-echo $fns
 # NOTE: assuming that julia executable is added to the path
 # export PATH="/path/to/julia/bin/julia_bin/julia-1.9.3/bin/:$PATH"
 # Also assuming julia version 1.9.3
-
 echo `which julia`
+
+fns="./code/*"
+echo $fns
+
+LOG_FILE="log.csv"
+
+# Function to check if an entry already exists in the log file
+entry_exists() {
+    local user="$1"
+    local year="$2"
+    local day="$3"
+    local language="$4"
+    grep -q "$user, $year, $day, $language" "$LOG_FILE"
+}
+
+check_solution() {
+    YEAR="$1"
+    DAY="$2"
+    RESULT1="$3"
+    RESULT2="$4"
+
+    # Query the solutions json file
+    SOLUTION1=$(cat ./data/solutions.json | jq -r ".\"$YEAR\".\"$DAY\"[0]")
+    SOLUTION2=$(cat ./data/solutions.json | jq -r ".\"$YEAR\".\"$DAY\"[1]")
+
+    # Test correctness of solution 1
+    if [ "$RESULT1" = "$SOLUTION1" ]; then
+        CORRECT1=true
+    else
+        CORRECT1=false
+    fi
+
+    # Test correctness of solution 2
+    if [ "$RESULT2" = "$SOLUTION2" ]; then
+        CORRECT2=true
+    else
+        CORRECT2=false
+    fi
+}
 
 for SCRIPT_FN in $fns
 do
     echo $SCRIPT_FN
+    # Extract user, YYYY, and D from the filename
+    FILENAME=$(basename "$SCRIPT_FN")
+    USER=$(echo "$FILENAME" | cut -d'_' -f1)
+    YEAR=$(echo "$FILENAME" | cut -d'_' -f2)
+    DAY=$(echo "$FILENAME" | cut -d'_' -f3 | cut -d'.' -f1)
 
-    # NOTE: this is super crappy programming, need to deal with this later
-    # string manipulation so that everything after the first underscore is cut off:
-    # ./code/DVH
-    DERP=${SCRIPT_FN%%_*}
-    # this get the number of letters in the string
-    # echo ${#DERP}
-    # we use the number of the sliced string above to slice the code file name
-    # and get someting of the form: YYYY_D or YYYY_DD
-    # echo ${SCRIPT_FN:${#DERP}+1:-3}
-    YEARDAY=${SCRIPT_FN:${#DERP}+1:-3}
-    # split again into the year and the day
-    YEAR=${YEARDAY%%_*}
-    DAY=${YEARDAY#*_}
-    # we add that slice to some other small string to get our data file location
-    DATAFILE="./data/input_"
-    DATAFILE+=$YEAR
-    DATAFILE+="_"
-    DATAFILE+=$DAY
-    DATAFILE+=".txt"
+    DATAFILE="./data/input_${YEAR}_${DAY}.txt"
 
-    RESULT=$(julia $SCRIPT_FN $DATAFILE)
-
-    # Splitting the tuple into two integers
-    RESULT=${RESULT#*=}
-    RESULT=${RESULT// /}
-    RESULT=${RESULT//(/}
-    RESULT=${RESULT//)/}
+    # Determine the language based on the file extension
+    EXTENSION="${FILENAME##*.}"
+    if [ "$EXTENSION" = "py" ]; then
+        LANGUAGE="python"
+        # Check if an entry with the same first 4 columns exists in the log file
+        if entry_exists "$USER" "$YEAR" "$DAY" "$LANGUAGE"; then
+            echo "Skipping execution for $USER, $YEAR, $DAY, $LANGUAGE as it already exists in the log."
+            continue  # Skip the current iteration and move to the next script
+        fi
+        # Run the Python command and capture its output
+        RESULT=$("$PYTHON_EXEC" "$SCRIPT_FN" "$DATAFILE")
+        # Remove leading and trailing parentheses and spaces
+        RESULT=${RESULT#*(}
+        RESULT=${RESULT%)*}
+        RESULT=${RESULT// /}
+        # Measure execution time and capture the result
+        EXECTIME=$({ TIMEFORMAT=%E; time $PYTHON_EXEC "$SCRIPT_FN" "$DATAFILE" > /dev/null; } 2>&1)
+    elif [ "$EXTENSION" = "jl" ]; then
+        # Check if an entry with the same first 4 columns exists in the log file
+        if entry_exists "$USER" "$YEAR" "$DAY" "$LANGUAGE"; then
+            echo "Skipping execution for $USER, $YEAR, $DAY, $LANGUAGE as it already exists in the log."
+            continue  # Skip the current iteration and move to the next script
+        fi
+        LANGUAGE="julia"
+        # Run the Julia command and capture its output
+        RESULT=$(julia "$SCRIPT_FN" "$DATAFILE")
+        # Splitting the tuple into two integers
+        RESULT=${RESULT#*=}
+        RESULT=${RESULT// /}
+        RESULT=${RESULT//(/}
+        RESULT=${RESULT//)/}
+        # Measure execution time and capture the result
+        EXECTIME=$({ TIMEFORMAT=%E; time julia "$SCRIPT_FN" "$DATAFILE" > /dev/null; } 2>&1)
+    else
+        LANGUAGE="unknown"
+        echo "Unknown file extension: $EXTENSION"
+    fi
+    # Split the result into RESULT1 and RESULT2 using a comma as the delimiter
     IFS=',' read -r RESULT1 RESULT2 <<< "$RESULT"
+    # Call the function with the appropriate arguments
+    check_solution "$YEAR" "$DAY" "$RESULT1" "$RESULT2"
 
-    echo "Result 1: $RESULT1"
-    echo "Result 2: $RESULT2"
-
-    # query the solutions json file
-    SOLUTION1=$(cat ./data/solutions.json | jq -r ".\"$YEAR\".\"$DAY\"[0]")
-    SOLUTION2=$(cat ./data/solutions.json | jq -r ".\"$YEAR\".\"$DAY\"[1]")
-    # test correctness of solution.
-    if [ "$RESULT1" = "$SOLUTION1" ]; then
-        echo "Solution 1 is correct."
-    fi
-    if [ "$RESULT2" = "$SOLUTION2" ]; then
-        echo "Solution 2 is correct."
-    fi
-
-    # timing of execution
-    # add another if clause here for python and julia
-    # TIMEFORMAT=%E get real execution time in seconds
-    EXECTIME=$({ TIMEFORMAT=%E; time julia $SCRIPT_FN $DATAFILE > /dev/null; } 2>&1)
+    echo $USER
+    echo $YEAR
+    echo $DAY
+    echo $LANGUAGE
+    echo $CORRECT1
+    echo $CORRECT2
     echo $EXECTIME
+
+    # Store the result in the log file
+    echo "$USER, $YEAR, $DAY, $LANGUAGE, $CORRECT1, $CORRECT2, $EXECTIME" >> "$LOG_FILE"
+
 done
